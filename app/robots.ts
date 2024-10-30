f57b98af50bd83a5cba5facbe4b6d3c849c911fa
Vlad <shutov.vladislav.2017@gmail.com>
2024-10-30 10:36:38 +0400

    feat: add useless robots file

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
             allow: '/',
             disallow: '/[lng]/dashboard/',
         },
-        sitemap: 'https://acme.com/sitemap.xml',
+        // sitemap: 'https://acme.com/sitemap.xml',
     }
 }
